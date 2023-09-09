0db5bc8366b7527cfb108fa5b85d59f5ae376a92
ansandi <an.sandi15@gmail.com>
2023-09-09 11:26:38 +0700

    initialization project

---
+++ src/javascript/main.js
@@ -0,0 +1,14 @@
+import Typed from "typed.js";
+
+new Typed("#heading", {
+  strings: [
+    "Hello There!",
+    "I am An Sandi",
+    "I am a Fullstack Frontend.",
+    "and Javascript Enthusiast",
+  ],
+  typeSpeed: 78,
+  backSpeed: 50,
+  loop: true,
+  cursorChar: "_",
+});
